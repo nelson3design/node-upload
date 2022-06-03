f0398442c75a10aea94cc4aa28c4a105461f872a
nelson3design <mandeart2@gmail.com>
2022-06-03 11:05:08 -0300

    final

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const exphbs = require('express-handlebars')
 
 const fileUpload = require('express-fileupload');
 
+const mysql = require('mysql');
+const Connection = require('mysql/lib/Connection');
 
 const app =express()
+app.use(fileUpload());
 
 const port = process.env.PORT || 5000;
 
@@ -14,20 +17,114 @@ app.engine('.hbs', handlebars.engine);
 app.set('view engine', '.hbs');
 
 
+app.use(express.static('public'))
+app.use(express.static('upload'))
 
 
-app.get('/',(req,res)=>{
-    res.render("index")
+
+
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host : 'localhost',
+    user : 'root',
+    password: '',
+    database: 'userprofile'
 })
 
-app.post('/',(req,res)=>{
-   let sampleFile;
-   let upladPath
 
-   if(!req.files || Object.keys(req.files).length ===0){
-       return res.status(400).send('No files were uploaded')
-   }
+pool.getConnection((err, connection)=>{
+    if(err) throw err
+    console.log('connected!')
 })
+
+
+
+
+
+
+
+
+
+app.get('',(req, res)=>{
+   
+
+    pool.getConnection((err, connection)=>{
+        if(err) throw err
+        console.log('connected!')
+
+
+        connection.query('SELECT * FROM user WHERE id=id',(err ,rows)=>{
+
+            connection.release()
+
+            if(!err){
+                res.render("index", { rows })
+            }
+        })
+    })
+    
+    
+
+})
+
+
+
+
+app.post('', (req, res) => {
+    let sampleFile;
+    let uploadPath;
+  
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
+  
+    
+    sampleFile = req.files.sampleFile;
+    uploadPath = __dirname + '/upload/' + sampleFile.name;
+
+   
+
+
+  
+    console.log(sampleFile);
+  
+   
+    sampleFile.mv(uploadPath, function (err) {
+      if (err) return res.status(500).send(err);
+
+
+        
+    //   res.send('file uploaded!')
+
+
+
+    pool.getConnection((err, connection)=>{
+        if(err) throw err
+        console.log('connected!')
+
+
+        connection.query('UPDATE user SET profile_image =? WHERE id=id',[sampleFile.name],(err ,rows)=>{
+
+            connection.release()
+
+            if(!err){
+                res.redirect('/')
+            }else{
+                console.log(err)
+            }
+        })
+    })
+       
+      });
+  });
+  
+
+
+
+
+
+
 
 app.listen(port,()=>{
     console.log(`servidor rodando na porta ${port}`)
